76f1290266fb862afa67ad8f07e090abe9e9506a
bmarticus <bmartsmail@gmail.com>
2017-10-02 18:37:54 -0400

    Initial Commit

---
+++ ShakemeGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShakemeGame
+//
+//  Created by Brian Martinez on 10/2/17.
+//  Copyright © 2017 Brian Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShakemeGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShakemeGame
+//
+//  Created by Brian Martinez on 10/2/17.
+//  Copyright © 2017 Brian Martinez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
